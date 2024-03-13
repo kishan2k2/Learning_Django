d565610126eeb024d16e9c9dbbaa8276edfb483e
Kishan <payadikishan@gmail.com>
2024-03-13 11:47:14 +0530

    learned about loose coupling

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # To view the index of the website.
     path("", views.index, name='index'),
     # To view the question detail i.e. /polls/5
-    path("<int:question_id>/", views.details, name = 'details'),
+    path("specifies/<int:question_id>/", views.details, name = 'details'),
     # To view the result.
     # /polls/question_id/result
     path('<int:question_id>/result', views.result, name = 'result'),
